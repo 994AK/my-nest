79f43756fd31ac8e99988e6c829f6db4a6318f28
994AK <1505398065@qq.com>
2023-05-03 09:20:08 +0800

    feat: 添加两个学习接口
    本次提交添加了两个新的学习接口：

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUser() {
+    return { name: '994AK', email: '1505398065@qq.com' };
+  }
+
+  @Post()
+  postUser(@Req() req: Request) {
+    // req.body 代表 传进来的参数
+    console.log(req.body);
+    return req.body;
+  }
+
+  @Get('/:userId')
+  getUserParam(@Param() param: { userId: number } ) {
+    return param;
   }
 }
